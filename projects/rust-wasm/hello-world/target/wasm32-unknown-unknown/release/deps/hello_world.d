9d34eb477c166005a4306e4299a9537072e06e38
zixuanzh <zixuanzh@seas.upenn.edu>
2018-07-23 23:13:33 -0400

    rust-wasm hello world

---
+++ projects/rust-wasm/hello-world/target/wasm32-unknown-unknown/release/deps/hello_world.d
@@ -0,0 +1,5 @@
+/Users/zzx/Desktop/blockchain/rust/projects/rust-wasm/hello-world/target/wasm32-unknown-unknown/release/deps/hello_world.wasm: src/lib.rs
+
+/Users/zzx/Desktop/blockchain/rust/projects/rust-wasm/hello-world/target/wasm32-unknown-unknown/release/deps/hello_world.d: src/lib.rs
+
+src/lib.rs:
---
+++ projects/rust-wasm/hello-world/target/wasm32-unknown-unknown/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/zzx/Desktop/blockchain/rust/projects/rust-wasm/hello-world/target/wasm32-unknown-unknown/release/hello_world.wasm: /Users/zzx/Desktop/blockchain/rust/projects/rust-wasm/hello-world/src/lib.rs
